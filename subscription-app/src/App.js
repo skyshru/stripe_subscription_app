d95b3674a20cb9abf984c9665d7aaa6a10dd4601
Shruti Sharma <shrutinautial@gmail.com>
2023-08-10 17:23:37 +0530

    importing plan to App.js

--- subscription-app/src/App.js
+++ subscription-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Register from './components/auth/register'; // Import the Register component
 import Login from './components/auth/login';
+import MonthlyPlanPage from './components/plans/monthyPlan';
 function App() {
   return (
     <div className="App">
       {/* Render the Register component */}
       
-      <Register/>
+      <MonthlyPlanPage/>
     </div>
   );
 }
