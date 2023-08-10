65a2af84549effa6eafe75e423642a839138bcfc
Shruti Sharma <shrutinautial@gmail.com>
2023-08-10 11:53:42 +0530

    The app.js file

---
+++ subscription-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Register from './components/auth/register'; // Import the Register component
+import Login from './components/auth/login';
+function App() {
+  return (
+    <div className="App">
+      {/* Render the Register component */}
+      
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
