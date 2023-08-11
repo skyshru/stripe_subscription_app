8dcf0ff71fb054eb0897a25444659a5ae656a57b
Shruti Sharma <shrutinautial@gmail.com>
2023-08-11 11:43:01 +0530

    Completed the UI

--- subscription-app/src/App.js
+++ subscription-app/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Register from './components/auth/register'; // Import the Register component
 import Login from './components/auth/login';
 import MonthlyPlanPage from './components/plans/monthyPlan';
+import CompletePayment from './components/CompletePayement/completePayement';
+import CurrentPlanActive from './components/plans/currentPlanActive';
+import CurrentPlanCancel from './components/plans/currentPlanCancel';
+
 function App() {
   return (
     <div className="App">
       {/* Render the Register component */}
       
-      <MonthlyPlanPage/>
+      <CurrentPlanCancel/>
     </div>
   );
 }
--- subscription-app/src/components/plans/monthyPlan.js
+++ subscription-app/src/components/plans/monthyPlan.js
@@ -6,15 +6,24 @@ const MonthlyPlanPage = () => {
   const [isYearly, setIsYearly] = useState(false);
 
   const plans = [
-    { id: 'mobile', title: 'MOBILE', price: '100', videoQuality: 'Good', resolution: '480p', devices: ['Phone'] },
-    { id: 'basic', title: 'BASIC', price: '200', videoQuality: 'Good', resolution: '480p', devices: ['Phone', 'Tablet'] },
-    { id: 'standard', title: 'STANDARD', price: '500', videoQuality: 'Better', resolution: '1080p', devices: ['Phone', 'Tablet', 'Computer'] },
-    { id: 'premium', title: 'PREMIUM', price: '700', videoQuality: 'Best', resolution: '4K+ HDR', devices: ['Phone', 'Tablet', 'Computer', 'TV'] },
+    { id: 'mobile', title: 'MOBILE', price: '1000', videoQuality: 'Good', resolution: '480p', devices: ['Phone'] },
+    { id: 'basic', title: 'BASIC', price: '2000', videoQuality: 'Good', resolution: '480p', devices: ['Phone', 'Tablet'] },
+    { id: 'standard', title: 'STANDARD', price: '5000', videoQuality: 'Better', resolution: '1080p', devices: ['Phone', 'Tablet', 'Computer'] },
+    { id: 'premium', title: 'PREMIUM', price: '7000', videoQuality: 'Best', resolution: '4K+ HDR', devices: ['Phone', 'Tablet', 'Computer', 'TV'] },
   ];
 
   const handlePlanSelect = (planId) => {
     setSelectedPlan(planId);
+  
+    // Remove the selected class from all td elements
+    const tds = document.querySelectorAll('.plans-table td');
+    tds.forEach(td => td.classList.remove('selected-column'));
+  
+    // Add the selected class to the corresponding td elements
+    const selectedColumn = document.querySelectorAll(`.${planId}-column`);
+    selectedColumn.forEach(td => td.classList.add('selected-column'));
   };
+  
 
   const handleSwitchToggle = () => {
     setSelectedPlan(null); // Reset selected plan when toggling switch
@@ -25,47 +34,69 @@ const MonthlyPlanPage = () => {
     <div className="monthly-plan-page">
       <h2>Choose the right plan for you</h2>
       <div className="switch-container">
-        <label className={`switch-label ${isYearly ? 'active' : ''}`}>Monthly</label>
-        <label className="switch">
-          <input type="checkbox" checked={isYearly} onChange={handleSwitchToggle} />
-          <span className="slider round"></span>
-        </label>
-        <label className={`switch-label ${isYearly ? '' : 'active'}`}>Yearly</label>
+        <div className="switch-labels">
+          <label className={`switch-label ${isYearly ? 'active' : ''}`} onClick={() => setIsYearly(false)}>Monthly</label>
+          <label className="switch">
+            <input type="checkbox" checked={isYearly} onChange={handleSwitchToggle} />
+            <span className="slider round"></span>
+          </label>
+          <label className={`switch-label ${isYearly ? '' : 'active'}`} onClick={() => setIsYearly(true)}>Yearly</label>
+        </div>
+        <div className="plan-options">
+  {plans.map(plan => (
+    <div
+      key={plan.id}
+      className={`plan-box ${selectedPlan === plan.id ? 'selected' : ''} ${plan.id}`}
+      onClick={() => handlePlanSelect(plan.id)}
+    >
+      {plan.title}
+    </div>
+  ))}
+</div>
+
       </div>
       <table className="plans-table">
-        <thead>
-          <tr>
-            <th></th>
-            {plans.map(plan => (
-              <th key={plan.id} className={selectedPlan === plan.id ? 'selected' : ''}>
-                {plan.title}
-              </th>
-            ))}
-          </tr>
-        </thead>
         <tbody>
           <tr>
             <td>Monthly price</td>
             {plans.map(plan => (
-              <td key={plan.id}>₹{isYearly ? parseInt(plan.price) * 10 : plan.price}</td>
+              <td key={plan.id} className={`${selectedPlan === plan.id ? 'selected' : ''}`}>
+                ₹{isYearly ? parseInt(plan.price) * 12 : plan.price}
+              </td>
             ))}
           </tr>
           <tr>
-            <td>Video quality</td>
-            {plans.map(plan => (
-              <td key={plan.id}>{plan.videoQuality}</td>
-            ))}
-          </tr>
+  <td>Monthly price</td>
+  {plans.map(plan => (
+    <td
+      key={plan.id}
+      className={`${selectedPlan === plan.id ? 'selected' : ''} ${plan.id}-column`}
+    >
+      <span className="price">
+        ₹{isYearly ? parseInt(plan.price) * 12 : plan.price}
+      </span>
+    </td>
+  ))}
+</tr>
+
           <tr>
             <td>Resolution</td>
             {plans.map(plan => (
-              <td key={plan.id}>{plan.resolution}</td>
+              <td key={plan.id} className={`${selectedPlan === plan.id ? 'selected' : ''}`}>
+                {plan.resolution}
+              </td>
             ))}
           </tr>
           <tr>
             <td>Devices you can use to watch</td>
             {plans.map(plan => (
-              <td key={plan.id}>{plan.devices.join(', ')}</td>
+              <td key={plan.id} className={`${selectedPlan === plan.id ? 'selected' : ''}`}>
+                <ul className="devices-list">
+                  {plan.devices.map(device => (
+                    <li key={device}>{device}</li>
+                  ))}
+                </ul>
+              </td>
             ))}
           </tr>
         </tbody>
